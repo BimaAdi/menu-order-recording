9131685edd3d9fdcfb782b2eed085acb12003334
BimaAdi <bimaadi419@gmail.com>
2021-08-08 11:46:32 +0700

    remve unused

--- menu/serializers.py
+++ menu/serializers.py
@@ -6,13 +6,4 @@ class MenuSerializers(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['id', 'name', 'price']
-
-    def create(self, validated_data):
-        return Menu.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.price = validated_data.get('price', instance.price)
-        instance.save()
-        return instance
         
